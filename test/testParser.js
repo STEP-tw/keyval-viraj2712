378ae693958934b7bad8d9244995d7b9793e98d0
viraj <virajpatil2712@gmail.com>
2018-01-08 11:04:38 +0530

    Installed chai and changed assert to chai assert. Also changed the old assert as simpleAssert.

--- test/testParser.js
+++ test/testParser.js
@@ -1,7 +1,9 @@
 const src=function(filePath){return "../src/"+filePath};
 const errors=function(filePath){return "../src/errors/"+filePath};
 
-const assert=require('assert');
+const simpleAssert = require('assert');
+
+const assert=require('chai').assert;
 const Parser=require(src('index.js')).Parser;
 const MissingValueError=require(errors('missingValueError.js'));
 const MissingEndQuoteError=require(errors('missingEndQuoteError.js'));
