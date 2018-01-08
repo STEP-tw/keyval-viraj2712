dd1d7884d856035c29cece2be2624cfe1604eb96
viraj <virajpatil2712@gmail.com>
2018-01-08 17:50:36 +0530

    Added tests for case insensitivity of strict key value parser.

--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -19,12 +19,84 @@ describe("strict parser that is case insensitive",function(){
     let parsed=kvParser.parse("name=jayanth");
     assert.deepEqual(parsed,expected);
   });
+  it("should parse when specified keys are in upper and lower case and actual is not",function(){
+    let kvParser=new StrictParser(["NAME","name"],false);
+    let expected=new Parsed();
+    expected["nAmE"]="jayanth";
+    let parsed=kvParser.parse("nAmE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper case with underScore and actual is different",function(){
+    let kvParser=new StrictParser(["_NAME"],false);
+    let expected=new Parsed();
+    expected["_nAmE"]="jayanth";
+    let parsed=kvParser.parse("_nAmE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in lower case with underScore and actual is different",function(){
+    let kvParser=new StrictParser(["_name"],false);
+    let expected=new Parsed();
+    expected["_NaMe"]="jayanth";
+    let parsed=kvParser.parse("_NaMe=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper and lower case with underScore and actual is different",function(){
+    let kvParser=new StrictParser(["_nAmE"],false);
+    let expected=new Parsed();
+    expected["_nAmE"]="jayanth";
+    let parsed=kvParser.parse("_nAmE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in lower case with numbers and actual is different",function(){
+    let kvParser=new StrictParser(["name123"],false);
+    let expected=new Parsed();
+    expected["nAmE123"]="jayanth";
+    let parsed=kvParser.parse("nAmE123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper case with numbers and actual is different",function(){
+    let kvParser=new StrictParser(["NAME123"],false);
+    let expected=new Parsed();
+    expected["NaMe123"]="jayanth";
+    let parsed=kvParser.parse("NaMe123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper and lower case with numbers and actual is different",function(){
+    let kvParser=new StrictParser(["NaMe123"],false);
+    let expected=new Parsed();
+    expected["name123"]="jayanth";
+    let parsed=kvParser.parse("name123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper case with numbers,underscore and actual is different",function(){
+    let kvParser=new StrictParser(["NAME_123"],false);
+    let expected=new Parsed();
+    expected["naMe_123"]="jayanth";
+    let parsed=kvParser.parse("naMe_123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in lower case with numbers,underscore and actual is different",function(){
+    let kvParser=new StrictParser(["name_123","age_123"],false);
+    let expected=new Parsed();
+    expected["naMe_123"]="jayanth";
+    expected["AGe_123"]="31";
+    let parsed=kvParser.parse("naMe_123=jayanth AGe_123=31");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are in upper and lower case with numbers,underscore and actual is different",function(){
+    let kvParser=new StrictParser(["NaMe_123","age_123"],false);
+    let expected=new Parsed();
+    expected["naMe_123"]="jayanth";
+    expected["AGe_123"]="31";
+    let parsed=kvParser.parse("naMe_123=jayanth AGe_123=31");
+    assert.deepEqual(parsed,expected);
+  });
 });
+
 
 describe("strict parser that is case sensitive",function(){
   it("should throw error when specified keys are in lower case and actual is not",function(){
     let kvParser=new StrictParser(["name"],true);
-    // true indicates that parser is case sensitive
     assert.throws(()=>{
       kvParser.parse("NAME=jayanth");
     })
